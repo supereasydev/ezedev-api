147b8927092f0f8803018b235709979e72156da1
e-maks <matantsev0@gmail.com>
2023-05-23 20:25:23 +0300

    Create script to watch updates in master

---
+++ watcher.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Specify the name of the Systemd service
+SERVICE_NAME="ezedev"
+
+# Specify the path to the Git repository
+REPO_PATH="/opt/backend"
+
+# Navigate to the Git repository
+cd "$REPO_PATH"
+
+# Start an infinite loop
+while true; do
+  # Fetch the latest changes from the remote repository
+  git fetch
+
+  # Check if there are any new changes
+  if ! git diff --quiet HEAD origin/master; then
+    # Pull the latest changes from the remote repository
+    git pull
+
+    # Restart the Systemd service
+    systemctl restart "$SERVICE_NAME"
+
+    # Print a success message
+    echo "Updated Git repository and restarted $SERVICE_NAME"
+  fi
+
+  # Wait for a few seconds before checking for new changes again
+  sleep 10
+done
