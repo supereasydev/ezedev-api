6e1911fc0e45f2bef9dc06b6ccd30f457269ff8c
e-maks <matantsev0@gmail.com>
2023-05-23 20:19:09 +0300

    Add Http exceptions handler

--- src/routers/exception_handler.py
+++ src/routers/exception_handler.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
@@ -7,3 +8,7 @@ from src.routers.schemas import PostResult
 
 async def unicorn_error(request: Request, exc: UnicornException):
     return JSONResponse(content=PostResult(success=False, error=exc.error).__dict__)
+
+
+async def HTTPException_error(request: Request, exc: HTTPException):
+    return JSONResponse(content=dict(error=exc.detail), status_code=exc.status_code)
